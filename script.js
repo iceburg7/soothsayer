5b1423cbd7e103bb42572ba3e946146d8fc686bb
iceburg7 <ecburgess1@gmail.com>
2016-04-17 00:56:57 -0400

    start

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	
+	console.log("it works.");
+	
+	
+	var song1 = document.getElementById("song1");
+	
+	//song1.play();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
